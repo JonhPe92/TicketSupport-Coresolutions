1907653738720e65625615c918be7e6241e3a734
Jonnathan Peralta <jon.peralta92@gmail.com>
2023-02-01 00:06:46 -0500

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import ReactDOM from 'react-dom/client';
+
+// third party
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
+// project imports
+import * as serviceWorker from 'serviceWorker';
+import App from 'App';
+import { store } from 'store';
+
+// style + assets
+import 'assets/scss/style.scss';
+
+// ==============================|| REACT DOM RENDER  ||============================== //
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+            console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+        );
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const httpLink = new HttpLink({ uri: process.env.REACT_APP_GRAPHQL_URI });
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const client = new ApolloClient({
+    link: from([errorLink, httpLink]),
+    cache: new InMemoryCache()
+});
+
+root.render(
+    <Provider store={store}>
+        <ApolloProvider client={client}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ApolloProvider>
+    </Provider>
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ src/themes/index.js
@@ -0,0 +1,55 @@
+import { createTheme } from '@mui/material/styles';
+
+// assets
+import colors from 'assets/scss/_themes-vars.module.scss';
+
+// project imports
+import componentStyleOverrides from './compStyleOverride';
+import themePalette from './palette';
+import themeTypography from './typography';
+
+/**
+ * Represent theme style and structure as per Material-UI
+ * @param {JsonObject} customization customization parameter object
+ */
+
+export const theme = (customization) => {
+    const color = colors;
+
+    const themeOption = {
+        colors: color,
+        heading: color.darkTextTitle,
+        paper: color.paper,
+        backgroundDefault: color.paper,
+        background: color.paper,
+        darkTextPrimary: color.grey200,
+        darkTextSecondary: color.grey300,
+        textDark: color.grey500,
+        menuSelected: color.secondaryDark,
+        menuSelectedBack: color.secondaryLight,
+        divider: color.secondaryLight,
+        customization
+    };
+
+    const themeOptions = {
+        direction: 'ltr',
+        palette: themePalette(themeOption),
+        mixins: {
+            toolbar: {
+                minHeight: '48px',
+                padding: '18px',
+                '@media (min-width: 600px)': {
+                    minHeight: '48px'
+                }
+            }
+        },
+        typography: themeTypography(themeOption)
+    };
+
+    const themes = createTheme(themeOptions);
+    themes.components = componentStyleOverrides(themeOption);
+
+    return themes;
+};
+
+export default theme;
